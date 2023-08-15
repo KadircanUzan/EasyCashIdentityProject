3b3034dbd6056de5edb1de0415832174ad9ee812
Kadircan UZAN <kadircanuzan@gmail.com>
2023-08-15 17:41:27 +0300

    Business Layer katmanın da şimdilik gerekli servisleri yazdık. Sayfalar için bazı temalar root klasörüne eklenildi.

---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountManager.cs
@@ -0,0 +1,40 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccountManager : ICustomerAccountService
+    {
+        private readonly ICustomerAccountDal _customerAccountDal;
+
+        public CustomerAccountManager(ICustomerAccountDal customerAccountDal)
+        {
+            _customerAccountDal = customerAccountDal;
+        }
+        public void TDelete(CustomerAccount entity)
+        {
+            _customerAccountDal.Delete(entity);
+        }
+
+        public CustomerAccount TGetById(int id)
+        {
+            return _customerAccountDal.GetById(id);
+        }
+
+        public List<CustomerAccount> TGetList()
+        {
+            return _customerAccountDal.GetList();
+        }
+
+        public void TInsert(CustomerAccount entity)
+        {
+            _customerAccountDal.Insert(entity);
+        }
+
+        public void TUpdate(CustomerAccount entity)
+        {
+            _customerAccountDal.Update(entity);
+        }
+    }
+}
